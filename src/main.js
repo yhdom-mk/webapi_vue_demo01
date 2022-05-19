fe69d67630885734ecf8186eec7e97d0779220dd
Mski Ydom <lcydome.msk12@gmail.com>
2022-05-20 05:32:32 +0900

    firebase initialize

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import firebase from 'firebase/compat/app';
 import './index.css'
+// import { initializeApp } from "firebase/app";
 
-createApp(App).mount('#app')
+///Firebase configuration///
+// /const firebaseConfig = process.env.VUE_APP_FIREBASE_CONFIG
+const firebaseConfig = {
+  
+};
+
+///Initialize Firebase///
+firebase.initializeApp(firebaseConfig);
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
+// const app = initializeApp(firebaseConfig);
+// createApp(App).mount('#app')
+
+
+
+
+
