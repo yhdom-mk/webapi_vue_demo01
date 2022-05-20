0002b0a89a54f28a838e4070e11ac32be5662f5c
Mski Ydom <lcydome.msk12@gmail.com>
2022-05-21 01:49:44 +0900

    ホーム画面 explain function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,7 @@
 <template>
   <h1>Home</h1>
+  <p>you can regist your favorit Album and Tune.</p>
+  <p>Let's manage them, as mach as you want.</p>
+
+  <p>あなたの好きなアルバムや楽曲を好きなだけ管理してみてください</p>
 </template>
