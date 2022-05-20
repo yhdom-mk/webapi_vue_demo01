7e3e55e88bda01de499b5bce0cd69ed8afa015b9
Mski Ydom <lcydome.msk12@gmail.com>
2022-05-20 21:22:23 +0900

    setup ref 修正　error 未解決

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,7 +1,6 @@
 <template>
   <h1>Album</h1>
-  <h3>This page is for users Only.</h3>
-  <h3>（このページはユーザ専用ページです）</h3>
+  <h3>This page is for users Only.（このページはユーザ専用ページです</h3>
   <div>
     <div>
       <button type="button"
@@ -45,7 +44,7 @@
                 v-on:keyup="FilterFn()"
                 placehoder="Filter"/>
                 <button type="button" class="btn btn-light"
-                v-on:click="sortResult('AlbumName', true)">
+                 @click="sortResult('AlbumName', true)">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 16 16">
                     <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
                     <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
@@ -53,7 +52,7 @@
                 </button>
 
                 <button type="button" class="btn btn-light"
-                v-on:click="sortResult('AlbumName', false)">
+                 @click="sortResult('AlbumName', false)">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-square" viewBox="0 0 16 16">
                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                     <path d="M3.544 10.705A.5.5 0 0 0 4 11h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5a.5.5 0 0 0-.082.537z"/>
@@ -68,15 +67,15 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(item, index) in albums" :key="index">
-            <td>{{ item.AlbumId }}</td>
-            <td>{{ item.AlbumName }}</td>
+          <tr v-for="abm in albums" :key="abm">
+            <td>{{ abm.AlbumId }}</td>
+            <td>{{ abm.AlbumName }}</td>
             <td>
               <button type="button"
                 class="btn btn-light mr-1"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
-                v-on:click="editClick(item)">
+                @click="editClick(abm)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -84,7 +83,7 @@
               </button>
               <button type="button"
                 class="btn btn-light mr-1"
-                v-on:click="deleteClick(item.AlbumId)">
+                @click="deleteClick(abm.AlbumId)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M6.5 1a.5.5 0 0 0-.5.5v1h4v-1a.5.5 0 0 0-.5-.5h-3ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1H3.042l.846 10.58a1 1 0 0 0 .997.92h6.23a1 1 0 0 0 .997-.92l.846-10.58Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                 </svg>
@@ -124,41 +123,125 @@
 </template>
 
 <script setup>
-// import { ref, onMounted} from '@vue/composition-api'
-import { ref, onMounted} from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import firebase from 'firebase/compat/app';
 import "firebase/compat/auth";
 import { useRouter } from 'vue-router';
-import { onBeforeUnmount } from 'vue';
+// import { onBeforeUnmount } from 'vue';
+// import { ref, onMounted} from '@vue/composition-api'
 
 const router = useRouter()
-  const authListener = firebase.auth().onAuthStateChanged(function(user) {
-    if(!user) {
-      alert(
-        'you must be logged in to view this. Redirecting to the home page'
-        + '（このページを見るためにはログインが必要です。ホーム画面にリダイレクトします）'
-      )
-      router.push('/')
+const authListener = firebase.auth().onAuthStateChanged(function(user) {
+  if(!user) {
+    alert(
+      'you must be logged in to view this. Redirecting to the home page\n'
+      +'（このページを見るためにはログインが必要です。ホーム画面にリダイレクトします）'
+    )
+    router.push('/')
+  }
+})
+
+onBeforeUnmount(()=> {
+  authListener()
+})
+
+const albums =ref([])
+const modalTitle =ref('')
+const AlbumName =ref('')
+const AlbumId =ref(0)
+const AlbumIdFilter =ref('')
+const AlbumNameFilter =ref('')
+const albumsWithoutFilter =ref([])
+
+const refreshData = ()=> {
+  axios.get(variables.API_URL + "/album")
+  .then((response) => {
+    albums.value = response.data;
+    albumsWithoutFilter.value = response.data;
+  });
+}
+
+const addClick = ()=> {
+  modalTitle.value ="Add Album";
+  AlbumId.value =0;
+  AlbumName.value ="";
+}
+
+const editClick = (abm)=> {
+  modalTitle.value ="Edit Album";
+  AlbumId.value =abm.AlbumId;
+  AlbumName.value =abm.AlbumName;
+},
+
+const createClick = ()=> {
+  axios.post(variables.API_URL + "/album", {
+    AlbumName: AlbumName.value
+  })
+  .then((response) => {
+    this.refreshData();
+    alert(response.data);
+  })
+},
+
+  const updateClick = ()=> {
+    axios.put(variables.API_URL + "/album", {
+      AlbumId: AlbumId.value,
+      AlbumName: AlbumName.value
+    })
+    .then((response) => {
+      this.refreshData();
+      alert(response.data);
+    })
+  },
+
+  const deleteClick = ()=> {
+    if(!confirm("Are you sure?")){
+      return;
     }
+    axios.delete(variables.API_URL + "/album/" + id)
+    .then((response) => {
+      this.refreshData();
+      alert(response.data);
+    });
+  },
+
+  const FilterFn = ()=> {
+    var AlbumIdFilter =AlbumIdFilter.value; //?????
+    var AlbumNameFilter =AlbumNameFilter.value
+    
+    albums.value = albumsWithoutFilter.value.filter(  //??おそらくこれできる
+      function(el) {
+        return el.AlbumId.toString().toLowerCase().includes(
+          AlbumIdFilter.toString().trim().toLowerCase()
+        )&&
+        el.AlbumName.toString().toLowerCase().includes(
+          AlbumNameFilter.toString().trim().toLowerCase()
+        )
+      }
+    )
+  },
+
+  const sortResult = (prop, asc)=> {
+    albums.value = albumsWithoutFilter.value.sort(function(a,b) {
+      if(asc) {
+        return (a[prop] >b[prop])? 1:((a[prop]< b[prop])? -1:0);
+      }
+      else {
+        return (b[prop] >a[prop])? 1:((b[prop]< a[prop])? -1:0);
+      }
+    })
+  }
+  
+  onMounted(()=> {
+    this.refreshData();
   })
 
-  onBeforeUnmount(()=> {
-    authListener()
-  })
 // export default {
 //   name: 'Album',
 //   // props: {
 //   //   msg: String
 //   // },
 //   setup() {
-    const albums =ref([]);
-    const modalTitle =ref('');
-    const AlbumName =ref('');
-    const AlbumId =ref(0);
-    const AlbumIdFilter =ref('');
-    const AlbumNameFilter =ref('');
-    const albumsWithoutFilter =ref([]);
-
 //     return {
 //       albums,
 //       modalTitle,
--- src/components/Tune.vue
+++ src/components/Tune.vue
@@ -1,14 +1,13 @@
 <template>
   <h1>Tune</h1>
-  <h3>This page is for users Only.</h3>
-  <h3>（このページはユーザ専用ページです）</h3>
+  <h3>This page is for users Only.（このページはユーザ専用ページです）</h3>
   <div>
     <div>
       <button type="button"
         class="btn btn-primary m-2 float-end"
         data-bs-toggle="modal"
         data-bs-target="#exampleModal"
-        v-on:click="addClick">
+        @click="addClick">
         Add Tune
       </button>
       <table class="table table-striped">
@@ -32,17 +31,17 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(item, index)in tunes" :key="index">
-            <td>{{ item.TuneId }}</td>
-            <td>{{ item.TuneName }}</td>
-            <td>{{ item.Album }}</td>
-            <td>{{ item.DateOfJoining }}</td>
+          <tr v-for="tun in tunes" v-bind:key="tun">
+            <td>{{ tun.TuneId }}</td>
+            <td>{{ tun.TuneName }}</td>
+            <td>{{ tun.Album }}</td>
+            <td>{{ tun.DateOfJoining }}</td>
             <td>
               <button type="button"
                 class="btn btn-light mr-1"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
-                v-on:click="editClick(item)">
+                v-on:click="editClick(tun)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -50,7 +49,7 @@
               </button>
               <button type="button"
                 class="btn btn-light mr-1"
-                v-on:click="deleteClick(item.TuneId)">
+                v-on:click="deleteClick(tun.TuneId)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M6.5 1a.5.5 0 0 0-.5.5v1h4v-1a.5.5 0 0 0-.5-.5h-3ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1H3.042l.846 10.58a1 1 0 0 0 .997.92h6.23a1 1 0 0 0 .997-.92l.846-10.58Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                 </svg>
@@ -80,8 +79,8 @@
               <div class="input-group mb-3">
                 <span class="input-group-text">Album</span>
                 <select class="form-select" v-model="Album">
-                  <option v-for="(item, index) in albums" :key="index">
-                    {{ item.AlbumName }}
+                  <option v-for="abm in albums" :key="abm">
+                    {{ abm.AlbumName }}
                   </option>
                 </select>
               </div>
@@ -112,42 +111,126 @@
 </template>
 
 <script setup>
-import { ref, onMounted} from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import firebase from 'firebase/compat/app';
 import "firebase/compat/auth";
 import { useRouter } from 'vue-router';
-import { onBeforeUnmount } from 'vue';
+// import { onBeforeUnmount } from 'vue';
 
 const router = useRouter()
-  const authListener = firebase.auth().onAuthStateChanged(function(user) {
-    if(!user) {
-      alert(
-        'you must be logged in to view this. Redirecting to the home page'
-        + '（このページを見るためにはログインが必要です。ホーム画面にリダイレクトします）'
-      )
-      router.push('/')
-    }
-  })
+const authListener = firebase.auth().onAuthStateChanged(function(user) {
+  if(!user) {
+    alert(
+      'you must be logged in to view this. Redirecting to the home page\n'
+      + '（このページを見るためにはログインが必要です。ホーム画面にリダイレクトします）'
+    )
+    router.push('/')
+  }
+})
 
-  onBeforeUnmount(()=> {
-    authListener()
+onBeforeUnmount(()=> {
+  authListener()
+})
+
+const tunes =ref([]);
+const albums =ref([]);
+const modalTitle =ref('');
+const TuneId =ref(0);
+const TuneName =ref('');
+const Album =ref('');
+const DateOfJoining =ref('');
+const PhotoFileName = ref("anonymous.png"),
+// const PhotoPath = ref(variables.PHOTO_URL)
+
+const refreshData = ()=> {
+  axios.get(variables.API_URL + "/tune")
+  .then((response) => {
+    tunes.value = response.data;
+    // this.tunes = response.data;
+  });
+}
+
+const addClick = ()=> {
+  modalTitle.value ="Add Tune";  // this.modalTitle 
+  TuneId.value =0; // this.TuneId =0;
+  TuneName.value ="";  // this.TuneName ="";
+  Album.value ="";  // this.Album ="",
+  DateOfJoining.value ="";  // this.DateOfJoining ="",
+  PhotoFileName.value ="anonymous.png"; // this.PhotoFileName ="anonymous.png"
+}
+
+const editClick = (tun)=> {
+  modalTitle.value ="Edit Tune";
+  TuneId.value =tun.TuneId;
+  TuneName.value =tun.TuneName;
+  Album.value =tun.Album,
+  DateOfJoining.value =tun.DateOfJoining,
+  PhotoFileName.value =tun.PhotoFileName
+}
+
+const createClick = ()=> {
+  axios.post(variables.API_URL + "/tune", {
+    TuneName: TuneName.value,
+    Album: Album.value,
+    DateOfJoining: DateOfJoining.value,
+    PhotoFileName: PhotoFileName.value
   })
+  .then((response) => {
+    this.refreshData();
+    alert(response.data);
+  })
+}
+
+const updateClick = ()=> {
+  axios.put(variables.API_URL + "/tune", {
+    TuneId: TuneId.value,
+    TuneName: TuneName.value,
+    Album: Album.value,
+    DateOfJoining: DateOfJoining.value,
+    PhotoFileName: PhotoFileName.value
+  })
+  .then((response) => {
+    this.refreshData();
+    alert(response.data);
+  })
+}
+
+const deleteClick = ()=> {
+  if(!confirm("Are you sure?")){
+    return;
+  }
+  axios.delete(variables.API_URL + "/tune/" + id)
+  .then((response) => {
+    this.refreshData();
+    alert(response.data);
+  });
+}
+
+const imageUpload = (event)=> {
+  let formData = new FormData();
+  formData.append('file', event.target.files[0]);
+  axios.post(
+    variables.API_URL + "tune/savefile",
+    formData
+  )
+  .then((response)=> {
+    PhotoFileName.value = response.data;
+    // this.PhotoFileName= response.data;
+  });
+}
+
+onMounted(()=> {
+  this.refreshData();
+})
+
 // export default {
 //   name: 'Tune',
 //   // props: {
 //   //   msg: String
 //   // },
 //   setup() {
-    const tunes =ref([]);
-    const albums =ref([]);
-    const modalTitle =ref('');
-    const TuneId =ref(0);
-    const TuneName =ref('');
-    const Album =ref('');
-    const DateOfJoining =ref('');
     // const PhotoFileName =ref('anonymous.png');
     // const PhotoPath =variables.PHOTO_URL
-
 //     return {
 //       tunes,
 //       albums,
